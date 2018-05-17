f1007c75dd6ee9a5e5f8e5f6b3bec95a568f8280
ThisIsJustUs <hebenstreit.justus@gmail.com>
2018-05-17 21:54:55 +0200

    LALA

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,22 @@
 //Price Range
-
+$(function() {
+  $("#slider-range").slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [75, 300],
+    slide: function(event, ui) {
+      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+    }
+  });
+  $("#min-range").val("$" + $("#slider-range").slider("values", 0) +
+    " - $" + $("#slider-range").slider("values", 1));
+});
 
 //Slick Carousel
-$(document).ready(function(){
+$(document).ready(function() {
   $('.listing-carousel').slick({
     arrows: true,
   });
 });
+// Check
